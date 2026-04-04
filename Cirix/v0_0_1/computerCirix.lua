56134f985f220cbf7d005e4eff018dd645d0f583
Chitak 985 <156968477+Chitak985@users.noreply.github.com>
2026-04-04 12:53:05 -0400

    Create computerCirix.lua

---
+++ Cirix/v0_0_1/computerCirix.lua
@@ -0,0 +1,2 @@
+-- wget https://raw.githubusercontent.com/Chitak985/chitak-oc-projects/refs/heads/main/Cirix/v0_0_1/computerCirix.lua && computerCirix
+while(true) do if(component.redstone.getInput(3) > 0) then component.assembler.start() end end
