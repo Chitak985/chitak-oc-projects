9cde19595a1f676b3a9c77723a55676f8dc1f57a
Chitak 985 <156968477+Chitak985@users.noreply.github.com>
2026-04-04 21:45:03 -0400

    computerCirix: report when done assembling

--- Cirix/v0_0_1/computerCirix.lua
+++ Cirix/v0_0_1/computerCirix.lua
@@ -1,2 +1,14 @@
 -- wget https://raw.githubusercontent.com/Chitak985/chitak-oc-projects/refs/heads/main/Cirix/v0_0_1/computerCirix.lua && computerCirix
-while(true) do if(component.redstone.getInput(3) > 0) then component.assembler.start() end end
+while(true) do
+  if(component.redstone.getInput(3) > 0) then         -- Recieved request to assemble
+    component.assembler.start()                       -- Assemble
+    while(component.assembler.status() == "busy") do  -- Wait until finished
+      
+    end
+    component.redstone.setOutput(3,1)                 -- Report that assembling finished
+    while(component.redstone.getInput(3) > 0) do      -- Wait until confirmation
+
+    end
+    component.redstone.setOutput(3,0)                 -- Stop reporting
+  end
+end
