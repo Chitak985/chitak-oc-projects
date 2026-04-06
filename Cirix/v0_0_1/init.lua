4e7722a6eb2742d61503615d44c63abda0d061ac
Chitak 985 <156968477+Chitak985@users.noreply.github.com>
2026-04-06 14:14:38 -0400

    Update submind init with startup code

--- Cirix/v0_0_1/init.lua
+++ Cirix/v0_0_1/init.lua
@@ -1,7 +1,23 @@
 -- init.lua file for the Cirix Submind, not the actual Cirix itself, use main.lua instead.
-robot = component.require("robot")
-roobt.turnAround()
-roobt.turnAround()
-roobt.turnAround()
-roobt.turnAround()
-roobt.turnAround()
+do
+  local addr, invoke = computer.getBootAddress(), computer.invoke
+  local function loadfile(file)
+    local handle = assert(invoke(addr, "open", file))
+    local buffer = ""
+    repeat
+      local data = invoke(addr, "read", handle, math.maxinteger or math.huge)
+      buffer = buffer .. (data or "")
+    until not data
+    invoke(addr, "close", handle)
+    return load(buffer, "=" .. file, "bt", _G)
+  end
+  loadfile("/lib/core/boot.lua")(loadfile)
+end
+
+local component = require("component")
+local robot = component.robot
+robot.turnAround()
+robot.turnAround()
+robot.turnAround()
+robot.turnAround()
+robot.turnAround()
