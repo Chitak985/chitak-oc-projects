1a3cb646f5253d3c1583c1ad2c392b485aff0d74
Chitak 985 <156968477+Chitak985@users.noreply.github.com>
2026-04-06 14:05:12 -0400

    Create init.lua for cirix submind

---
+++ Cirix/v0_0_1/init.lua
@@ -0,0 +1,7 @@
+-- init.lua file for the Cirix Submind, not the actual Cirix itself, use main.lua instead.
+robot = component.require("robot")
+roobt.turnAround()
+roobt.turnAround()
+roobt.turnAround()
+roobt.turnAround()
+roobt.turnAround()
