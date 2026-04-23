08f71831980c7a0e74bb149d83b257f182604400
Chitak 985 <156968477+Chitak985@users.noreply.github.com>
2026-04-23 09:59:07 -0400

    Rename init(submind) to autorun(submind)

---
+++ Cirix/v0_0_1/autorun.lua
@@ -0,0 +1,8 @@
+-- autorun.lua file for the Cirix Submind, not the actual Cirix itself, use main.lua instead.
+local component = require("component")
+local robot = component.robot
+robot.turnAround()
+robot.turnAround()
+robot.turnAround()
+robot.turnAround()
+robot.turnAround()
